13486437f4713ecebac3e057c488b753f0accdce
NorelFarjun <norelp123@gmail.com>
2022-05-27 10:45:25 +0300

    init

---
+++ main.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchFrameException
+import sys
+import time
+from datetime import date
+from datetime import datetime
+
+
+PATH = "chromedriver.exe"
+options = webdriver.ChromeOptions()
+
+
+
+def main():
+    options.add_argument(r'--user-data-dir=')
+    driver = webdriver.Chrome(PATH, options= options)
+    driver.get("https://web.whatsapp.com/")
+    time.sleep(20)
+
+
+
+if __name__ == '__main__':
+    main()
+
